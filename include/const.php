4e2bdd8dbb1d9dcfd7c893048794c4fb8c8d1fe2
ankitasharma2698 <67278462+ankitasharma2698@users.noreply.github.com>
2020-06-23 15:26:00 +0530

    Add files via upload

---
+++ include/const.php
@@ -0,0 +1,24 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "heartcare_db");
+$dbname = DB_NAME;
+$con;
+
+define("EM_HOST", "smtp.gmail.com");
+define("EM_PORT", 587);
+define("EM_FROM_NAME", "Heart Care");
+define("EM_SECURITY", 'tls');
+define("EXIT_ON_ERROR", false);
+//change me
+define("EM_FROM", "sharmaankita2611@gmail.com");
+define("EM_PASS", '26111998@');
+
+define("ADMIN_EMAIL", 'sharmaankita2611@gmail.com');
+define("EMAIL_MSG", 'Mail from website contact us!');
+$base_url = "http://localhost/heart/";
+$con;
+
+
+?>
